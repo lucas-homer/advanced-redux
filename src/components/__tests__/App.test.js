89b55f9ec674a6808c76a4ba849d5b62e72c2775
Lucas Homer <lucas.homer@gmail.com>
2018-08-11 16:09:49 -0500

    committing to push and clone for new version starting in unit 3

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import App from '../App';
 
-import App from '../App';
+// it('shows a comment box', () => {
+// 	const wrapped = shallow(<App />);
 
-it('shows a comment box', () => {
-	const div = document.createElement('div');
 
-	ReactDOM.render(<App />, div);
-	// looks inside the div
-	// and checks to see if the CommentBox is in there
-	expect(div.innerHTML).toContain('Comment Box');
 
-	ReactDOM.unmountComponentAtNode(div);
-});
+// });
