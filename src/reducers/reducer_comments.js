1eb613a6b5dfa9255d9e93b80898cf5309b9eb81
Lucas Homer <lucas.homer@gmail.com>
2018-08-11 16:50:46 -0500

    fix a few typos

--- src/reducers/reducer_comments.js
+++ src/reducers/reducer_comments.js
@@ -4,7 +4,9 @@ export default function(state = [], action) {
 	switch(action.type) {
 		case SAVE_COMMENT:
 			return [ ...state, action.payload ];
+		default:
+			return state;
 	}
 
-	return state;
+	
 }
