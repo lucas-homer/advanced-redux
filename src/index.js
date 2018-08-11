ebe526e0a1dff76a5dfec33a2d7e8babb2e242bf
Lucas Homer <lucas.homer@gmail.com>
2018-08-11 17:16:51 -0500

    add root file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
+import Root from './Root';
 import App from './components/App';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
+	<Root>
 		<App />
-	</Provider>
+	</Root>
 	, document.querySelector('#root'));
